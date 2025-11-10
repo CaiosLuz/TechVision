3ff6489448b2cabb74953288f96ebbff67d5ae52
Caio José <caiojsdl16@gmail.com>
2025-11-09 22:58:36 -0300

    tela de produto detalhe atualizada para buscar infos dos produtos

--- src/app/produto-detalhe/produto-detalhe.ts
+++ src/app/produto-detalhe/produto-detalhe.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProdutoService } from '../services/produto';
 import { MenuSuperior } from '../menu-superior/menu-superior';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-produto',
   standalone: true,
-  imports: [CommonModule, MenuSuperior], // <- Adicione o CommonModule aqui
+  imports: [CommonModule, FormsModule, MenuSuperior, Footer],
   templateUrl: './produto-detalhe.html',
   styleUrls: ['./produto-detalhe.css'],
 })
@@ -15,24 +17,32 @@ export class InfoProduto implements OnInit {
   produto: any;
   imagens: string[] = [];
   imagemPrincipal = '';
+  quantidade = 1;
 
   constructor(
     private produtoService: ProdutoService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   async ngOnInit() {
-    try {
-      const idStr = this.route.snapshot.paramMap.get('id')!;
-      const id = Number(this.route.snapshot.paramMap.get('id'));
-      this.produto = await this.produtoService.getProduto(id);
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.produto = await this.produtoService.getProduto(id);
+    this.imagens = this.produto?.imagens_produto?.map((i: any) => i.url) || [];
+    this.imagemPrincipal = this.imagens[0] || '';
+  }
 
-      // Configura imagens
-      this.imagens = this.produto?.imagens_produto?.map((i: any) => i.url) || [];
-      this.imagemPrincipal = this.imagens[0] || '';
-      console.log('Produto:', this.produto);
-    } catch (err) {
-      console.error(err);
-    }
+  alterarQuantidade(valor: number) {
+    this.quantidade = Math.max(1, this.quantidade + valor);
+  }
+
+  adicionarAoCarrinho() {
+    alert(`Adicionado ${this.quantidade}x ${this.produto.nome} ao carrinho 🛒`);
+    // Aqui você poderia salvar no localStorage ou Supabase
+  }
+
+  comprarAgora() {
+    alert(`Compra imediata de ${this.quantidade}x ${this.produto.nome} ⚡`);
+    this.router.navigate(['/venda']);
   }
 }
